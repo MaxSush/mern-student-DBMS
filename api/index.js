7ede27bf183ff43c47b521c49c7ea3784708c145
MaxSush <psushant880@gmail.com>
2023-12-17 13:27:09 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,23 @@
 import express from 'express'
+import { createConnection } from 'mysql';
+import dotenv from 'dotenv'
+dotenv.config();
 
+const db = createConnection({
+    host: process.env.env_host,
+    user: process.env.env_user,
+    password: process.env.env_pass,
+    })
+
+db.connect((err) => { 
+    if (err) { 
+      console.log("Database Connection Failed !!!", err); 
+    } else { 
+      console.log("connected to Database"); 
+    } 
+});                                  
 const app = express();
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000 !!!');
